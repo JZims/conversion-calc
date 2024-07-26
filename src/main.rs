b7444e503be9417c8777ee596ea1205488d5db20
JZims <zimmerman.j@hotmail.com>
2024-07-25 23:39:38 -0400

    input type needs error handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use clap::Parser;
 
+
 #[derive(Parser)]
 struct Cli {
-    /// The pattern to look for.
-    pattern: String, 
-    /// The path to the file being read. 
-    path: std::path::PathBuf
+    /// Either Volume or Weight.
+    conversion_input: String, 
+    /// Modifier used for material.
+    gravity_mod: f32
 }
 
 fn main () {
     let args = Cli::parse();
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+    if args.conversion_input.to_lowercase() == "volume" {
+        println!("You chose Volume! Your incoming value should be in mL and will be converted to g.")
+    }
+    if args.conversion_input.to_lowercase() == "weight" {
+        println!("You chose weight! Your incoming value should be in g and will be converted to mL.")
+    }
+
 }
 
 
