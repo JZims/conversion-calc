0293edb21e3090a108bdbb4c925409f6bd58c623
JZims <zimmerman.j@hotmail.com>
2024-07-25 21:51:13 -0400

    basic arguments and return set up

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for.
+    pattern: String, 
+    /// The path to the file being read. 
+    path: std::path::PathBuf
+}
+
+fn main () {
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+}
+
+
+
