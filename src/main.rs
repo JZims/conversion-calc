b64a76dd5cc6bac4d578945511a8919cfefa948b
JZims <zimmerman.j@hotmail.com>
2024-07-30 00:19:01 -0400

    basic functionality working

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use clap::{command, Arg, Parser, Command};
 fn main() {
     let conversion = command!()
             .arg(
-                Arg::new("gravity_modifer")
+                Arg::new("gravity_modifier")
                 .short('g')
                 .long("gravity-mod")
                 .alias("grav")
@@ -20,7 +20,7 @@ fn main() {
                 .long("type")
                 .alias("type")
                 .help("What is the incomming value to be converted measured in? ('weight' or 'volume')")
-                // .value_parser(["weight", "volume", "Weight", "Volume", "g", "ml"])
+                .value_parser(["weight", "volume", "Weight", "Volume", "g", "ml"])
                 .global(true)
 
              )
@@ -32,15 +32,33 @@ fn main() {
                 .alias("units")
                 .help("How many units are being converted into the opposite measurement?")
                 .global(true)
+                .value_parser(clap::value_parser!(f32))
 
              )
             .get_matches();
+
+    let modifier = conversion.get_one::<f32>("gravity_modifier").unwrap();
+    let c_type = conversion.get_one::<String>("conversion_type").unwrap();
+    let quantity = conversion.get_one::<f32>("unit_quantity").unwrap();
+
+    if c_type == "weight" {
+
+        let value:f32 = quantity / modifier;
+
+        println!("The converted weight of {}g to volume(ml) is: {}ml", quantity, value )
+
+    } else if c_type == "volume" {
+
+         let value:f32 = quantity * modifier;
+
+        println!("The converted volume of {}ml to weight(g) is: {}g", quantity, value )
+    }
     
-    println!("{}", conversion.get_one::<String>("conversion-type").unwrap())
+   
 
 }
 
-// Using Parser
+// Using built-in Parser
 
 // #[derive(Parser)]
 // struct Cli {
